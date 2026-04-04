e4c0610db6ec2878e49a9a6c11b0c21f2c293ed6
ayush-KUMAR-behera <ayushkumarbehera0237@gmail.com>
2026-04-04 20:46:17 +0530

    Added logging to patient service

--- src/main/java/com/ayush/serviceimpl/PatientServiceImpl.java
+++ src/main/java/com/ayush/serviceimpl/PatientServiceImpl.java
@@ -2,6 +2,9 @@ package com.ayush.serviceimpl;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.ayush.dto.PatientRequestDTO;
@@ -17,12 +20,15 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PatientServiceImpl implements PatientService {
 
-
+	private static final Logger logger=LoggerFactory.getLogger(PatientServiceImpl.class);
 	private final PatientRepository repo;
 
 
 	@Override
 	public List<PatientResponseDTO> getAllPatient() {
+		
+		logger.info("Fetching all active patients");
+		
 		List<Patient> list=repo.findByDeletedFalse();
 		List<PatientResponseDTO> dtolist=new ArrayList<PatientResponseDTO>();
 		for(Patient p:list) {
@@ -38,11 +44,26 @@ public class PatientServiceImpl implements PatientService {
 
 	@Override
 	public PatientResponseDTO getPatientById(Long id) {
-		Patient p=repo.findById(id).orElseThrow(()-> new ResourceNotFoundException("Patient not found with this id"));
+		
+		logger.info("Fetching patient with id: {}",id);
+		
+		Patient p = repo.findById(id)
+	            .orElseThrow(() -> {
+	            	
+	                logger.error("Patient not found with id: {}", id);
+	                
+	                return new ResourceNotFoundException("Patient not found");
+	            });
 		
 		if(p.getDeleted()) {
+			
+			logger.warn("Attemt to access deleted patient with id: {}", id);
+			
 			throw new ResourceNotFoundException("Patient not found");
 		}
+		
+
+		
 		PatientResponseDTO dto=new PatientResponseDTO();
 		dto.setId(p.getId());
 		dto.setName(p.getName());
@@ -53,6 +74,9 @@ public class PatientServiceImpl implements PatientService {
 
 	@Override
 	public PatientResponseDTO createPatient(PatientRequestDTO dto) {
+		
+		logger.info("creating new patient: {}",dto.getName());
+		
 	  Patient patient=new Patient();
 	  patient.setName(dto.getName());
 	  patient.setAge(dto.getAge());
@@ -68,10 +92,25 @@ public class PatientServiceImpl implements PatientService {
 
 	@Override
 	public void updatePatient(Long id, PatientRequestDTO dto) {
-		Patient p=repo.findById(id).orElseThrow(()-> new ResourceNotFoundException("Patient not found with this id"));
+		
+		logger.info("Updating patients:{}",dto.getName());
+		
+		Patient p=repo.findById(id).orElseThrow(()->{
+			
+			logger.error("Patient not found with with id:{}",id);
+			
+			return new ResourceNotFoundException("Patient not found with this id");
+		});
+		
+		
 		if(p.getDeleted()) {
+			
+			logger.warn("Attempt to update deleted patient with id: {}",id);
+			
 			throw new ResourceNotFoundException("Delete Patient can't update ");
 		}
+		
+		
 		
 		p.setAge(dto.getAge());
 		p.setName(dto.getName());
@@ -81,12 +120,24 @@ public class PatientServiceImpl implements PatientService {
 
 	@Override
 	public void deletePatient(Long id) {
-		Patient p=repo.findById(id).orElseThrow(()-> new ResourceNotFoundException("Patient not found with this id"));
+		Patient p=repo.findById(id).orElseThrow(()->{
+			
+		logger.error("Patient not found with id:{}",id);	
+			
+		return new ResourceNotFoundException("Patient not found with this id");
+		});
+		
 		if(p.getDeleted()) {
+			
+		logger.warn("Attempt to access deleted patient with id: {}", id);
+			
 			throw new ResourceNotFoundException("Patient already deleted");
 		}
+		
 		p.setDeleted(true);
 		repo.save(p);
+		
+		logger.info("Soft deleting patient with id: {}",id);
 		
 	}
 
